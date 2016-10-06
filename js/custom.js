8317d429656f81d4c6b01cde9fcb9948c9013e79
Simon Marriott <s.marriott783@btinternet.com>
2016-10-06 17:33:07 +0100

    Add contet in places for Venice and Rome

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,17 @@
  * Custom v1.0.
  * by Simon Marriott
  *
- * Copyright 2016, 2015 SJMWebDesigns, All rights reserved
+ * Copyright 2016, SJMWebDesigns, All rights reserved
  */
+
+
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+
+$(document).ready(function () {
+    'use strict';
+    var idToToggle = window.location.hash.replace("#", "");
+    $("#" + idToToggle).collapse('show');
+});
+
