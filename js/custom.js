062eccc623b58e9210508d92d606171bbae32cb0
Simon Marriott <s.marriott783@btinternet.com>
2016-10-09 18:37:47 +0100

    Some Styling changes

--- js/custom.js
+++ js/custom.js
@@ -16,5 +16,15 @@ $(document).ready(function () {
     $("#" + idToToggle).collapse('show');
 });
 
-/* Accordion Icon */
 
+/* Accordion Icon */
+jQuery(function ($) {
+    'use strict';
+    var $active = $('#accordion .panel-collapse.in').prev().addClass('active');
+    $active.find('a').append('<span class="accordion-icon"><i class="glyphicon glyphicon-minus"></i></span>');
+    $('#accordion .panel-heading').not($active).find('a').append('<span class="accordion-icon"><i class="glyphicon glyphicon-plus"></i></span>');
+    $('#accordion').on('show.bs.collapse', function (e) {
+        $('#accordion .panel-heading.active').removeClass('active').find('.glyphicon').toggleClass('glyphicon-plus glyphicon-minus');
+        $(e.target).prev().addClass('active').find('.glyphicon').toggleClass('glyphicon-plus glyphicon-minus');
+    });
+});
