d2847026d32bb8bce840b73763719260d7e3492f
Simon Marriott <s.marriott783@btinternet.com>
2016-10-17 15:45:38 +0100

    added pull down menu for places on the places page; added back to top functionality

--- js/custom.js
+++ js/custom.js
@@ -34,3 +34,35 @@ $(function ($) {
     });
 });
 /* /Accordion Icon */
+
+
+/* Scroll to Top */
+$(document).ready(function () {
+    'use strict';
+    var verticalOffset, offset, offsetTop, element;
+ 
+    function scrollToTop() {
+        verticalOffset = typeof (verticalOffset) !== 'undefined' ? verticalOffset : 0;
+        element = $('body');
+        offset = element.offset();
+        offsetTop = offset.top;
+        $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+    }
+
+    $(function () {
+
+        $(document).on('scroll', function () {
+            if ($(window).scrollTop() > 175) {
+                $('.scroll-top-wrapper').addClass('show');
+            } else {
+                $('.scroll-top-wrapper').removeClass('show');
+            }
+        });
+
+        $('.scroll-top-wrapper').on('click', scrollToTop);
+    });
+
+});
+
+
+
