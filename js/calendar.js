5a159c5693f57efcf2de6c5ec8f60c30966af162
Simon Marriott <s.marriott783@btinternet.com>
2016-11-07 16:38:23 +0000

    add clock and calendar to dashboard

---
+++ js/calendar.js
@@ -0,0 +1,24 @@
+//this function will set the text value of tags
+function setText(id, val) {
+    'use strict';
+    if (val < 10) {
+        val = '0' + val;    //add leading 0 if val < 10
+    }
+    document.getElementById(id).innerHTML = val;
+}
+
+//this function will find today's date
+function calendar() {
+    'use strict';
+    var day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        date = new Date();
+
+    setText('calendar-day', day[date.getDay()]);
+    setText('calendar-date', date.getDate());
+    setText('calendar-month-year', month[date.getMonth()] + ' ' + (1900 + date.getYear()));
+}
+
+
+//call calendar() when page load
+window.onload = calendar;
--- js/clock.js
+++ js/clock.js
@@ -1,23 +1,27 @@
-var ang, color, context, radius, roman, num, rom = false;
+var color, ctx, radius,
+    roman, num, canvas,
+    romanNumerals = false; //Set to true for Roman Numerals
 
-function drawFace(context, radius) {
+function drawFace(ctx, radius) {
     'use strict';
     var grad;
-    context.beginPath();
-    context.arc(0, 0, radius, 0, 2 * Math.PI);
-    context.fillStyle = '#ccc';
-    context.fill();
-    grad = context.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
-    grad.addColorStop(0, '#333');
-    grad.addColorStop(0.5, '#FFF');
-    grad.addColorStop(1, '#333');
-    context.strokeStyle = grad;
-    context.lineWidth = radius * 0.08;
-    context.stroke();
-    context.beginPath();
-    context.arc(0, 0, radius * 0.08, 0, 2 * Math.PI);
-    context.fillStyle = '#333';
-    context.fill();
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = '#ccc';
+    ctx.fill();
+    grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+    //Create a metallic copper gradient
+    grad.addColorStop(0.000, 'rgba(151, 70, 26, 1.000)');
+    grad.addColorStop(0.300, 'rgba(251, 216, 197, 1.000)');
+    grad.addColorStop(0.830, 'rgba(108, 46, 22, 1.000)');
+    grad.addColorStop(1.000, 'rgba(239, 219, 205, 1.000)');
+    ctx.strokeStyle = grad;
+    ctx.lineWidth = radius * 0.08;
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(0, 0, radius * 0.08, 0, 2 * Math.PI);
+    ctx.fillStyle = '#333';
+    ctx.fill();
 }
 
 function numToRoman(num) {
@@ -50,80 +54,103 @@ function numToRoman(num) {
     }
 }
 
-function drawNumbers(context, radius) {
+function drawNumbers(ctx, radius) {
     'use strict';
-    context.font = radius * 0.15 + "px times";
-    context.textBaseline = "middle";
-    context.textAlign = "center";
+    var ang, sang, cang, i, sx, sy, ex, ey, nx, ny;
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    
+    //Markings/Numerals
+    for (i = 1; i <= 60; i += 1) {
+        num = i / 5;
+        ang = Math.PI / 30 * i;
+        sang = Math.sin(ang);
+        cang = Math.cos(ang);
 
-    for (num = 1; num < 13; num += 1) {
-        roman = numToRoman(num);
-        ang = num * Math.PI / 6;
-        context.rotate(ang);
-        context.translate(0, -radius * 0.85);
-        context.rotate(-ang);
-        //context.fillText(num.toString(), 0, 0);
-        if (rom) {
-            context.fillText(roman, 0, 0);
+        //If i is an exact multiple of 5, draw the hour ticks & numerals
+        if (i % 5 === 0) {
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = '#000';
+            sx = sang * (radius * 0.78);
+            sy = cang * (radius * -0.78);
+            ex = sang * (radius * 0.95);
+            ey = cang * (radius * -0.95);
+            nx = sang * (radius * 0.65);
+            ny = cang * (radius * -0.65);
+            // Place numerals
+            if (romanNumerals) {
+                ctx.font = radius * 0.14 + "px times";
+                roman = numToRoman(num);
+                ctx.fillText(roman, nx, ny);
+            } else {
+                ctx.font = radius * 0.15 + "px arial";
+                ctx.fillText(num.toString(), nx, ny);
+            }
+        // Otherwise draw the minute ticks
         } else {
-            context.fillText(num.toString(), 0, 0);
+            ctx.lineWidth = 1;
+            sx = sang * radius * 0.85;
+            sy = cang * radius * 0.85;
+            ex = sang * radius * 0.95;
+            ey = cang * radius * 0.95;
+            ctx.strokeStyle = '#000';
         }
-        context.rotate(ang);
-        context.translate(0, radius * 0.85);
-        context.rotate(-ang);
+        ctx.beginPath();
+        ctx.moveTo(sx, sy);
+        ctx.lineTo(ex, ey);
+        ctx.stroke();
     }
 }
 
-function drawHand(context, pos, length, width, color) {
+function drawHand(ctx, pos, length, width, color) {
     'use strict';
-    context.strokeStyle = color;
-    context.beginPath();
-    context.lineWidth = width;
-    context.lineCap = "round";
-    context.moveTo(0, 0);
-    context.rotate(pos);
-    context.lineTo(0, -length);
-    context.stroke();
-    context.rotate(-pos);
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.moveTo(0, 0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
 }
 
-
-function drawTime(context, radius) {
+function drawTime(ctx, radius) {
     'use strict';
     var now = new Date(),
         hour = now.getHours(),
         minute = now.getMinutes(),
         second = now.getSeconds();
 
-    //hour
+    // Hour
     color = '#000';
     hour = hour % 12;
     hour = (hour * Math.PI / 6) +
         (minute * Math.PI / (6 * 60)) +
         (second * Math.PI / (360 * 60));
-    drawHand(context, hour, radius * 0.65, radius * 0.06, color);
+    drawHand(ctx, hour, radius * 0.65, radius * 0.06, color);
 
-    //minute
-    color = '000';
+    // Minute
+    color = '#000';
     minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
-    drawHand(context, minute, radius * 0.85, radius * 0.04, color);
+    drawHand(ctx, minute, radius * 0.85, radius * 0.04, color);
 
-    // second
+    // Second
     color = '#FF0000';
     second = (second * Math.PI / 30);
-    drawHand(context, second, radius * 0.9, radius * 0.02, color);
+    drawHand(ctx, second, radius * 0.9, radius * 0.02, color);
 }
 
 function drawClock() {
     'use strict';
-    drawFace(context, radius);
-    drawNumbers(context, radius);
-    drawTime(context, radius);
+    drawFace(ctx, radius);
+    drawNumbers(ctx, radius);
+    drawTime(ctx, radius);
 }
 
-var canvas = document.getElementById("canvas"),
-    context = canvas.getContext("2d"),
-    radius = canvas.height / 2;
-context.translate(radius, radius);
+canvas = document.getElementById("canvasTime");
+ctx = canvas.getContext("2d");
+radius = canvas.height / 2;
+ctx.translate(radius, radius);
 radius = radius * 0.90;
 setInterval(drawClock, 1000);
